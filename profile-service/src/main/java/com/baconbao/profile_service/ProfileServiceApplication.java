16a43a61757ae6fd39c9bbd51a21ac06ea8a83f3
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-07-26 16:58:14 +0700

    chung

---
+++ profile-service/src/main/java/com/baconbao/profile_service/ProfileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.baconbao.profile_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileServiceApplication.class, args);
+	}
+
+}
---
+++ profile-service/src/test/java/com/baconbao/profile_service/ProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baconbao.profile_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
