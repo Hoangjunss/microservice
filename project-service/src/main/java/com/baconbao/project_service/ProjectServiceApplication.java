7f68734e5dc486fb2077db11f7e330f992fd04eb
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-07-27 11:33:34 +0700

    chung

---
+++ project-service/src/main/java/com/baconbao/project_service/ProjectServiceApplication.java
@@ -0,0 +1,13 @@
+package com.baconbao.project_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectServiceApplication.class, args);
+	}
+
+}
---
+++ project-service/src/test/java/com/baconbao/project_service/ProjectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baconbao.project_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
