f12b6043e906a227f0db9b448d49a8d0a1da0f27
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-07-28 10:57:21 +0700

    chung

--- discovery-server/src/main/java/com/example/discovery_server/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/example/discovery_server/DiscoveryServerApplication.java
@@ -2,7 +2,9 @@ package com.example.discovery_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServerApplication {
 
