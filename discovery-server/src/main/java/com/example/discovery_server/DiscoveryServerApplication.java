ef795a114a8d19e591fac0ff3c5afc61dff7aae6
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-07-28 10:45:27 +0700

    chung

---
+++ discovery-server/src/main/java/com/example/discovery_server/DiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.discovery_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+}
