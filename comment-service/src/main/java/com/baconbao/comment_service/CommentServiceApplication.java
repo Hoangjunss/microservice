e590671c51629debaeb03da886f46908abdfb16d
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-07-28 12:43:20 +0700

    chung

--- comment-service/src/main/java/com/baconbao/comment_service/CommentServiceApplication.java
+++ comment-service/src/main/java/com/baconbao/comment_service/CommentServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CommentServiceApplication {
 
 	public static void main(String[] args) {
--- project-service/src/main/java/com/baconbao/project_service/ProjectServiceApplication.java
+++ project-service/src/main/java/com/baconbao/project_service/ProjectServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProjectServiceApplication {
 
 	public static void main(String[] args) {
