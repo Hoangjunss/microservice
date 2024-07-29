6690f57e6f56f8de347658857a9037d5624aa80a
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-07-29 09:36:45 +0700

    chung

---
+++ profile-service/src/main/java/com/baconbao/profile_service/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.baconbao.profile_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
--- profile-service/src/main/java/com/baconbao/profile_service/controller/ProfileController.java
+++ profile-service/src/main/java/com/baconbao/profile_service/controller/ProfileController.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class ProfileController {
     @Autowired
     private ProfileService profileService;
+    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/save")
     public ResponseEntity<ProfileDTO> save(@RequestBody ProfileDTO profileDTO){
         return ResponseEntity.ok(profileService.saveProfile(profileDTO));
