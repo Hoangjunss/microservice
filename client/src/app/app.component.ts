a910d757b56c0b9198f4cf9984927764467b0099
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-07-29 13:30:30 +0700

    chung

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { ProfileListComponent } from './profile-list/profile-list.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CreateProfileComponent } from './create-profile/create-profile.component';
 import { FormsModule } from '@angular/forms';
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ProfileListComponent,CreateProfileComponent],
+  imports: [RouterOutlet,ProfileListComponent,CreateProfileComponent,ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- client/src/app/profile.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Profile } from './profile';
-
-describe('Profile', () => {
-  it('should create an instance', () => {
-    expect(new Profile()).toBeTruthy();
-  });
-});
