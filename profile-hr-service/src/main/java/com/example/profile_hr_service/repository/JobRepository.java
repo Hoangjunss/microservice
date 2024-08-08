c3c8991838801890b3cfb6e142f1d64c62bae756
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-08-08 10:52:05 +0700

    chung

--- profile-hr-service/src/main/java/com/example/profile_hr_service/repository/JobRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.profile_hr_service.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.profile_hr_service.model.Job;
-
-public interface JobRepository extends MongoRepository<Job, Integer>{
-    
-}
--- profile-hr-service/src/main/java/com/example/profile_hr_service/services/serviceimpl/JobServiceImpl.java
+++
@@ -1,5 +0,0 @@
-package com.example.profile_hr_service.services.serviceimpl;
-
-public class JobServiceImpl {
-    
-}
