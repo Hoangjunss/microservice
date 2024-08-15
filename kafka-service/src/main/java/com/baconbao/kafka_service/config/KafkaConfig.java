85d4277b36f19025bfd282990d41f7efe9d94df6
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-08-15 13:15:04 +0700

    chung

--- kafka-service/src/main/java/com/baconbao/kafka_service/config/KafkaConfig.java
+++ kafka-service/src/main/java/com/baconbao/kafka_service/config/KafkaConfig.java
@@ -26,7 +26,7 @@ public class KafkaConfig {
     @Bean
     public ProducerFactory<String, MessageDTO> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:8089");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
